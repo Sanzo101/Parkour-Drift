23c4b5b5116cb36dbbede26ba0a6e5f10f5bc9c6
Sanzo101 <60393729+Sanzo101@users.noreply.github.com>
2021-10-26 13:03:48 +0100

    Player/Camera

---
+++ Parkour Drift/Assets/PlayerScripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    GameObject Player;
+    Rigidbody RB;
+    public string s_Player;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag(s_Player);
+        RB = Player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RB.AddForce(Vector3.forward * Speed);
+        }
+    }
+}
