701dd40da885462517084b21ad06ead903f670cb
Sanzo101 <60393729+Sanzo101@users.noreply.github.com>
2021-10-26 14:25:38 +0100

    PowerBar

---
+++ Parkour Drift/Assets/PlayerScripts/LaunchBar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaunchBar : MonoBehaviour
+{
+    public Image Mask;
+    public float Speed = 1;
+    float BarMaxValue = 100;
+    float BarCurrentValue;
+    bool BarIncreasing; 
+    [System.NonSerialized]
+    public bool BarOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BarCurrentValue = 0;
+        BarIncreasing = true;
+        BarOn = true;
+        StartCoroutine(UpdateBar());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator UpdateBar()
+    {
+        while (BarOn)
+        {
+            if(!BarIncreasing)
+            {
+                BarCurrentValue -= Speed;
+                if(BarCurrentValue<=0)
+                {
+                    BarIncreasing = true;
+                }
+            }
+            if(BarIncreasing)
+            {
+                BarCurrentValue += Speed;
+                if(BarCurrentValue>=BarMaxValue)
+                {
+                    BarIncreasing = false;
+                }
+            }         
+            float Fill = BarCurrentValue / BarMaxValue;
+            Mask.fillAmount = Fill;
+            yield return new WaitForSeconds(0.02f);
+        }
+        yield return null;
+    }
+
+}
--- Parkour Drift/Assets/PlayerScripts/Movement.cs
+++ Parkour Drift/Assets/PlayerScripts/Movement.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     GameObject Player;
+    public GameObject Bar;
     Rigidbody RB;
     public string s_Player;
     public float Speed;
+    LaunchBar PowerBar;
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag(s_Player);
         RB = Player.GetComponent<Rigidbody>();
+        PowerBar = Player.GetComponent<LaunchBar>();
     }
 
     // Update is called once per frame
@@ -21,6 +24,13 @@ public class Movement : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             RB.AddForce(Vector3.forward * Speed);
+            PowerBar.BarOn = false;
+            StartCoroutine(BarDisable());
         }
+    }
+    IEnumerator BarDisable()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Bar.SetActive(false);
     }
 }
